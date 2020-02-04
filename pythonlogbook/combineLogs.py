a92665be1536d59a98c6abbd16a3263cf6619929
lukew3 <lukew25073@gmail.com>
2020-02-04 18:46:32 -0500

    reversed parameters lists

--- pythonlogbook/combineLogs.py
+++ pythonlogbook/combineLogs.py
@@ -1,30 +1,39 @@
 # the goal of this program is to combine all logs into one log file
+#The code below works like this:
+#a list is made that contains all the contents of a folder
+#The list is then sorted so that the files that start with lower numbers are put at the front and files with higher numbers are put at the back
+#The while loop then travels up the list, starting from the bottom and does the same thing for subfolders
+#Once the date folder is entered, the files are read and written to output folder
+
 import os
 
 originalPath = r"/home/luke/Documents/logbook"
 saveLocation = r"/home/luke/Documents"
 outputFileLocation = "/home/luke/Documents" + "/output.txt"
 
-yearParameters = ["monthFolders", "yearFolderPath", "originalPath", "yearFolders", "i", "monthParameters"]
-monthParameters = ["dateFolders", "monthFolderPath", "yearFolderPath", "monthFolders", "j", "dateParameters"]
-dateParameters = ["entries", "dateFolderPath", "monthFolderPath", "dateFolders", "k", "none"]
+yearParameters = []
+monthParameters = []
+dateParameters = []
 
 def makeList(ogPath, prevList, counter):
         #print("Entering: " + prevList[counter])
+        #print(counter)
+        #print(prevList)
         nextPath = ogPath + "/" + prevList[counter]
+        print(nextPath)
         newList = os.listdir(nextPath)
         newList.sort()
         return newList, nextPath
 
-def makeFolderList(nextFolderList, currentPath, lastPath, currentFolderList, counterName, nextParamList):
-    counterName = 0
-    while counterName < len(currentFolderList):
-        nextFolderList, currentPath = makeList(lastPath, currentFolderList, counterName)
+def makeFolderList(nextFolderList, currentPath, lastPath, currentFolderList, nextParamList): #nextListName
+    count = 0
+    while count < len(currentFolderList):
+        nextFolderList, currentPath = makeList(lastPath, currentFolderList, count)
         if nextParamList != "none":
             makeFolderList(*nextParamList)
         elif nextParamList == "none":
             addData(nextFolderList)
-        counterName = counterName + 1
+        count = count + 1
 
 def addData(entries):
     d = 0
@@ -48,21 +57,23 @@ def main():
         open(outputFileLocation, 'a').close()
         print("Output.txt created")
 
-
-#The code below works like this:
-#a list is made that contains all the contents of a folder
-#The list is then sorted so that the files that start with lower numbers are put at the front and files with higher numbers are put at the back
-#The while loop then travels up the list, starting from the bottom and does the same thing for subfolders
-#Once the date folder is entered, the files are read and written to output folder
-
     yearFolders = os.listdir(originalPath)
     yearFolders.sort() #Sort in Ascending numeric order
+
+    dateParameters = ["entries", "dateFolderPath", "monthFolderPath", "dateFolders", "none"]
+    monthParameters = ["dateFolders", "monthFolderPath", "yearFolderPath", "monthFolders", dateParameters]
+    yearParameters = ["monthFolders", "yearFolderPath", originalPath, yearFolders, monthParameters]
+
 
     makeFolderList(*yearParameters)
 
     print("Logs combined")
 
 main()
+
+
+#monthParameters = [dateFolders, "monthFolderPath", "yearFolderPath", monthFolders, dateParameters]
+#dateParameters = [entries, "dateFolderPath", "monthFolderPath", dateFolders, "none"]
 #Sample output format
 #year
 
